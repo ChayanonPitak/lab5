26320f1a67581c826a15b46c4fe9d564e790756a
IkaWaAyuMu <pungpond4450@gmail.com>
2020-12-15 15:30:35 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int evenAmount = 0, oddAmount = 0;
+    int tempInteger;
+    while (true)
+    {
+        cout << "Enter an integer: ";
+        cin >> tempInteger;
+        if (tempInteger == 0) break;
+        else if (tempInteger % 2 == 0) evenAmount++;
+        else oddAmount++;
+    }
+    cout << "#Even numbers = " << evenAmount << endl;
+    cout << "#Odd numbers = " << oddAmount;
     return 0;
 }
